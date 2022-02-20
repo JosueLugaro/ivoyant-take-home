93dee5bd98e233b30202db9f25dd0e6d04b99f04
Josue Lugaro <josuelugaro15@gmail.com>
2022-02-19 20:36:00 -0600

    Implemented both a json response and an xml response, client is receiving the appropriate data, just need to sort the received data

---
+++ api/src/app.ts
@@ -0,0 +1,91 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+const jsonDelay: number = 5000;
+const xmlDelay: number = 10000
+
+const jsonObj = {
+    "person": [
+
+        {
+
+            "id": 10,
+
+            "firstName": "John",
+
+            "lastName": "Doe"
+
+        },
+
+        {
+
+            "id": 5,
+
+            "firstName": "Jack",
+
+            "lastName": "Doe"
+
+        },
+
+        {
+
+            "id": 7,
+
+            "firstName": "James",
+
+            "lastName": "Doe"
+
+        }
+
+    ]
+}
+
+const data = `<?xml version="1.0" encoding="UTF-8" ?>
+                <persons>
+                    <person>
+
+                        <id>3</id>
+
+                        <firstName>Jen</firstName>
+
+                        <lastName>Doe</lastName>
+
+                    </person>
+
+                    <person>
+
+                        <id>6</id>
+
+                        <firstName>Stephanie</firstName>
+
+                        <lastName>Joe</lastName>
+
+                    </person>
+
+                    <person>
+
+                        <id>1</id>
+
+                        <firstName>Victoria</firstName>
+
+                        <lastName>Doe</lastName>
+
+                    </person>
+                </persons>
+    `;
+
+app.get('/api/json', (req: Request, res: Response) => {
+    setTimeout((() => {
+        res.json(jsonObj);
+    }), jsonDelay);
+});
+
+app.get('/api/xml', (req: Request, res: Response) => {
+    setTimeout((() => {
+        res.type("application/xml");
+        res.send(data);
+    }), xmlDelay);
+});
+
+app.listen(5000, () => console.log("server is running"));
